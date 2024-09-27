06ae57f7d7d27d0f98b0f33fde0b03891d9bf6c6
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-09-27 16:04:47 +0200

    feat: add imageUrl param

--- models/Product.ts
+++ models/Product.ts
@@ -4,14 +4,16 @@ interface IProduct extends Document {
     name: string;
     description: string;
     price: number;
-    isFavourite: boolean;
+    isFavorite: boolean;
+    imageUrl: string
 }
 
 const ProductSchema: Schema = new Schema({
     name: { type: String, required: true },
     description: { type: String, required: false },
     price: { type: Number, required: true },
-    isFavourite: { type: Boolean, required: false }
+    isFavorite: { type: Boolean, required: false },
+    imageUrl: { type: String, required: false },
 });
 
 export default mongoose.model<IProduct>('Product', ProductSchema);
