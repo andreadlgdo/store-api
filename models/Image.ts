249e6ba0b1ffe6359a3364c1312055749851ed07
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-10-24 16:13:52 +0200

    feat: add device to image model

--- models/Image.ts
+++ models/Image.ts
@@ -3,11 +3,13 @@ import mongoose, { Schema, Document } from 'mongoose';
 interface IImage extends Document {
     type: string;
     impageUrl: string;
+    device: string;
 }
 
 const ImageSchema: Schema = new Schema({
     type: { type: String, required: true },
-    imageUrl: { type: String, required: true }
+    imageUrl: { type: String, required: true },
+    device: { type: String, required: true }
 });
 
 export default mongoose.model<IImage>('Image', ImageSchema);
