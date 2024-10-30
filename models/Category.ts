fa62144319d9e1a4cf4ec96c3aca717490002562
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-10-30 23:02:10 +0100

    feat: add categories

---
+++ models/Category.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface ICategory extends Document {
+    title: string;
+    impageUrl: string;
+}
+
+const CategorySchema: Schema = new Schema({
+    title: { type: String, required: true },
+    imageUrl: { type: String, required: true }
+});
+
+export default mongoose.model<ICategory>('Category', CategorySchema);
+
--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,5 @@
 export { default as authRoutes }  from "./authRoutes";
+export { default as categoryRoutes } from "./categoryRoutes";
 export { default as generalRoutes } from "./generalRoutes";
 export { default as imageRoutes } from "./imageRoutes";
 export { default as productRoutes } from './productRoutes';
