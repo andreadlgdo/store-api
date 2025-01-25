9d6e3f2c9744f849ac4282df52b8d77fdc0f6d80
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-01-25 15:37:35 +0100

    feat: add updateProduct method

--- controllers/productController.ts
+++ controllers/productController.ts
@@ -11,3 +11,35 @@ export const getProducts = async (req: Request, res: Response) => {
         res.status(500).json({ message: 'Error fetching products', error });
     }
 };
+
+export const updateProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        const { name, description, price, categories, quantity, imageUrl } = req.body;
+
+        const updateFields: { [key: string]: any } = {};
+
+        if (name) updateFields.name = name;
+        if (price) updateFields.price = price;
+        updateFields.description = description;
+        updateFields.categories = categories;
+        updateFields.quantity = quantity;
+        updateFields.imageUrl = imageUrl;
+
+        const updatedProduct = await Product.findByIdAndUpdate(
+            {_id: id},
+            { $set: updateFields },
+            { new: true, runValidators: true }
+        );
+
+
+        if (!updatedProduct) {
+            return res.status(404).json({ message: 'Producto no encontrado' });
+        }
+
+        res.json({ user: updatedProduct });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al actualizar un producto', error: error });
+    }
+}
--- routes/productRoutes.ts
+++ routes/productRoutes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
-import { getProducts } from '../controllers/productController';
+import { getProducts, updateProduct } from '../controllers/productController';
+
 
 const router = express.Router();
 
 router.get('/', getProducts);
+router.put('/:id', updateProduct);
 
 export default router;
