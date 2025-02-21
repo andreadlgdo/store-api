42aff82afd6e77d45050a7b892311db240b0159d
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-02-21 18:14:47 +0100

    fix: fix return of categories

--- models/Category.ts
+++ models/Category.ts
@@ -1,13 +1,15 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
-interface ICategory extends Document {
+export interface ICategory extends Document {
     title: string;
     imageUrl: string;
+    parentId?: string
 }
 
 const CategorySchema: Schema = new Schema({
     title: { type: String, required: true },
-    imageUrl: { type: String, required: true }
+    imageUrl: { type: String, required: true },
+    parentId: { type: String, required: false }
 });
 
 export default mongoose.model<ICategory>('Category', CategorySchema);
